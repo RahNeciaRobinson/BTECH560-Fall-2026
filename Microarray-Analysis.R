d79180c6f7ace2af7ff9e366363954f91bc7907c
Rah'Necia Robinson <rarobinson@claflin.edu>
2026-09-02 09:45:34 -0400

    Add files via upload

--- Microarray-Analysis.R
+++ Microarray-Analysis.R
@@ -33,3 +33,17 @@ Normalizeddata<-exprs(Normalizeddata)
 
 boxplot(Normalizeddata)
 
+#Assume first 2 chips are Cancer/Treatment type and the next 2 are Normal/Reference chips
+
+Treatment <-Normalizeddata[,c(1,2)]
+Control <-Normalizeddata[,c(3,4)]
+
+#Taking averages on rows for Treatment and Control so we have 1 column for Treatment and 1 column for Control.
+
+
+
+#Fold change analysis for cetermining significant genes. Up-regulation and Down-regulation of genes.
+#Fold changes can be performed using subtraction and division.
+
+Fold_Changes <-Treatment-Control
+
