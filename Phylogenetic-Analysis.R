d8193199134e18d338e02b7f122854d77e50d8bf
Rah'Necia Robinson <rarobinson@claflin.edu>
2026-09-11 09:38:51 -0400

    Add files via upload

---
+++ Phylogenetic-Analysis.R
@@ -0,0 +1,14 @@
+#Author:Robinson,Date:9/11/2026,Purpose:Phylogenetics analysis
+
+#Load the 4 libraries 
+
+library(ape)
+library(phangorn)
+library(phytools)
+library(geiger)
+
+#Create some dummy string dataset
+
+text.string<- "(((((((cow, pig),whale),(bat,(lemur,human))),(robin,iguana)),coelacanth),gold_fish),shark);"
+
+vert.tree<-read.tree(text=text.string)
