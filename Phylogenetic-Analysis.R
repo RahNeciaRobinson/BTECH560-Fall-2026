08a8af2b52755107ab32730965ea494f01efb776
Rah'Necia Robinson <rarobinson@claflin.edu>
2026-09-14 09:41:37 -0400

    Add files via upload

--- Phylogenetic-Analysis.R
+++ Phylogenetic-Analysis.R
@@ -12,3 +12,10 @@ library(geiger)
 text.string<- "(((((((cow, pig),whale),(bat,(lemur,human))),(robin,iguana)),coelacanth),gold_fish),shark);"
 
 vert.tree<-read.tree(text=text.string)
+
+#Plot the phylogenetic tree-Rooted Plot
+#Regenerate same plot using color "Red", and add title at the top stating "Rooted Phylogenetic Tree".
+plot(vert.tree,no.margin=TRUE,edge.width=2)
+
+plot(vert.tree,no.margin=TRUE,edge.color="red",main="Rooted Phylogenetic Tree",edge.width=2)
+
